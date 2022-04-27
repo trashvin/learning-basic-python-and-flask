5943ca6cada40efe0dd732928aef9328c65b933e
trashvin <m3lles@gmail.com>
2022-04-27 08:36:19 +0800

    changes

--- web_app_02/spock_app/game_engine.py
+++ web_app_02/spock_app/game_engine.py
@@ -9,6 +9,14 @@ def get_winner(player):
     variables = ['Rock', 'Paper', 'Scissor', 'Lizard', 'Spock']
     computer_choice = random.randrange(0,5)
     computer = variables[computer_choice]
+
+    # do you your changes below, replace the if condition that matches the rules
+
+    # Scissors cuts Paper,  Paper covers Rock,  Rock crushes Lizard,
+    # Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard,
+    # Lizard eats Paper,  Paper disproves Spock, Spock vaporizes Rock,
+    # (and as it always has) Rock crushes Scissors
+    
     if player > computer:
         return 'PLAYER',computer
     elif computer > player:
